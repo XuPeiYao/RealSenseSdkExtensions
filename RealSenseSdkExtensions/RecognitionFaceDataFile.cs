441d9ff1bfe8264c6d376982c8e6f5c9e982cc59
徐培堯 <st89382000@gmail.com>
2016-09-04 14:07:23 +0800

    臉部辨識資料結構二進制轉換支援

--- RealSenseSdkExtensions/RecognitionFaceDataFile.cs
+++ RealSenseSdkExtensions/RecognitionFaceDataFile.cs
@@ -10,7 +10,7 @@ namespace RealSenseSdkExtensions {
         /// <summary>
         /// 將臉部辨識資料結構陣列以二進制儲存至指定位置
         /// </summary>
-        /// <param name="dataArray">臉部辨識資料結構</param>
+        /// <param name="dataArray">臉部辨識資料結構陣列</param>
         /// <param name="path">檔案路徑</param>
         public static void Save(
             this RecognitionFaceData[] dataArray,
@@ -26,6 +26,21 @@ namespace RealSenseSdkExtensions {
         }
 
         /// <summary>
+        /// 將臉部辨識資料結構陣列轉換為二進制陣列
+        /// </summary>
+        /// <param name="dataArray">臉部辨識資料結構陣列</param>
+        /// <returns>二進制陣列</returns>
+        public static byte[] ToBinary(
+            this RecognitionFaceData[] dataArray) {
+            MemoryStream writer = new MemoryStream();
+            foreach (var item in dataArray) {
+                writer.Write(item.BinaryData,0,item.BinaryData.Length);
+                writer.Flush();
+            }
+            return writer.ToArray();
+        }
+
+        /// <summary>
         /// 自指定檔案讀取臉部辨識資料結構陣列
         /// </summary>
         /// <param name="path">檔案路徑</param>
@@ -38,7 +53,17 @@ namespace RealSenseSdkExtensions {
             while(reader.BaseStream.Position != reader.BaseStream.Length) {
                 buffer.Add(reader.ReadByte());
             }
-            return RecognitionFaceData.FromDatabaseBuffer(buffer.ToArray());
+            return Load(buffer.ToArray());
+        }
+
+        /// <summary>
+        /// 自二進制陣列讀取臉部辨識資料結構陣列
+        /// </summary>
+        /// <param name="binary">二進制陣列</param>
+        /// <returns>臉部辨識資料結構陣列</returns>
+        public static RecognitionFaceData[] Load(
+           byte[] binary) {
+            return RecognitionFaceData.FromDatabaseBuffer(binary);
         }
     }
 }
